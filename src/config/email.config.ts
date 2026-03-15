821d6ddd3a9519374772c70ad5ce2a9c31d18bb7
gedeon-gills <ndagedeo061@gmail.com>
2026-03-15 19:12:28 +0200

    forces ipv4

--- src/config/email.config.ts
+++ src/config/email.config.ts
@@ -1,22 +1,25 @@
 import nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
 import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.EMAIL_USER!,
+    pass: process.env.EMAIL_PASS!,
   },
-});
+  family: 4, // to force IPv4
+} as SMTPTransport.Options); // ✅ cast as SMTPTransport.Options
 
 transporter.verify((error) => {
   if (error) {
-    console.error('❌ Email Service Error:', error);
+    console.error("❌ Email Service Error:", error);
   } else {
-    console.log('✅ Email Service Ready');
+    console.log("✅ Email Service Ready");
   }
 });
-
 
 export default transporter;
