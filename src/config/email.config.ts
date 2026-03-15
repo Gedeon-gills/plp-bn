067e63f1ce308b2131aeb722ff77052614bbc435
gedeon-gills <ndagedeo061@gmail.com>
2026-03-15 19:29:23 +0200

    add new updates

--- src/config/email.config.ts
+++ src/config/email.config.ts
@@ -1,25 +1,22 @@
 import nodemailer from "nodemailer";
-import SMTPTransport from "nodemailer/lib/smtp-transport";
 import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER!,
-    pass: process.env.EMAIL_PASS!,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
-  family: 4, // to force IPv4
-} as SMTPTransport.Options); // ✅ cast as SMTPTransport.Options
+});
 
 transporter.verify((error) => {
   if (error) {
-    console.error("❌ Email Service Error:", error);
+    console.error('❌ Email Service Error:', error);
   } else {
-    console.log("✅ Email Service Ready");
+    console.log('✅ Email Service Ready');
   }
 });
+
 
 export default transporter;
